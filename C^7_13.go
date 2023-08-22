95ae4e640d194a13009e81f80f4b8825ed20c556
nazarovandry <43076457+nazarovandry@users.noreply.github.com>
2023-08-22 21:40:07 +0500

    Add files via upload

---
+++ C^7_13.go
@@ -0,0 +1,40 @@
+// Online Go compiler to run Golang program online
+// Print "Hello World!" message
+
+package main
+import "fmt"
+
+func next(prev bool, a int, b int) (int, int) {
+    add := 0
+    if (b > 0) {
+        second, second_count := next(false, a, b-1)
+        if prev {
+            add = 1
+        }
+        if (a == 0) {
+            return second + add * second_count, second_count
+        }
+        first, first_count := next(true, a-1, b)
+        return first + second + (1 - add) * first_count + add * second_count,
+            first_count + second_count
+    }
+    if (a > 0) {
+        first, first_count := next(true, a-1, b)
+        if !prev {
+            add = 1
+        }
+        return first + add * first_count, first_count
+    }
+    return 0, 1
+}
+
+func main() {
+    A := 7
+    B := 8
+    //sum, co := next(true, A-1, B) + next(false, A, B-1);
+    afirst, afirst_count := next(true, A-1, B)
+    asecond, asecond_count := next(false, A, B-1)
+    //fmt.Println(sum, co)
+    fmt.Println(afirst + asecond, afirst_count + asecond_count)
+    fmt.Println((afirst + asecond) / (afirst_count + asecond_count))
+}
